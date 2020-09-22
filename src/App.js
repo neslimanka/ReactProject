b1eb0650bb914e6860b02d409ae69ecbc3be8850
neslimanka <43746150+neslimanka@users.noreply.github.com>
2020-09-22 21:18:48 +0300

    reactstrap

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React, { Component } from 'react'
-import { Container, Row } from 'reactstrap'
-import CategoryList from './CategoryList'
-import Navi from './Navi'
-import Product from './Product'
+import React, { Component } from "react";
+import { Container, Row , Col} from "reactstrap";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import Product from "./Product";
 
 export default class App extends Component {
-    render() {
-        return (
-            <div>
-                <Container>
-                   <Row>
-                   <CategoryList></CategoryList>
-                   </Row>
-                   <Row>
-                       <Navi></Navi>
-                   </Row>
-                   <Product></Product>
-
-
-                </Container>
-                
-                <h3>Hello from React!</h3>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
+          <Row>
+            <Col xs="3">
+              <CategoryList />
+            </Col>
+            <Col xs="9">
+              <Product />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
