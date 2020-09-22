d7a15587f21a55abc612e57472d19c5a86f54824
neslimanka <43746150+neslimanka@users.noreply.github.com>
2020-09-22 23:18:02 +0300

    state

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,24 +1,29 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem} from "reactstrap";
+import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
+  //constructor(props) {
+   // super(props); //extend edilen component.
+    //this.state = {
+        state= {
+      catagories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ]
+    };
+  
 
-    constructor(props){
-        super(props);
-        state:{}
-    }
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
+        <h3>{this.state.counter}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {this.state.catagories.map((category) => (
+            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem> //key kavramı=her bir elemanı diğerinden ayıracak ve eşşis olacak bir ıd ver.
+          ))}
         </ListGroup>
-      </div> 
+      </div>
     );
   }
 }
