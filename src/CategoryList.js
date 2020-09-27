d46ca6eed1e7bf478e0c46458207cba63496fc8b
neslimanka <43746150+neslimanka@users.noreply.github.com>
2020-09-27 20:17:24 +0300

    ilkevent

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -3,16 +3,21 @@ import { ListGroup, ListGroupItem } from "reactstrap";
 
 export default class CategoryList extends Component {
   //constructor(props) {
-   // super(props); //extend edilen component.
-    //this.state = {
-        state= {
-      catagories: [
-        { categoryId: 1, categoryName: "Beverages" },
-        { categoryId: 2, categoryName: "Condiments" },
-      ]
-    };
-  
+  // super(props); //extend edilen component.
+  //this.state = {
+  state = {
+    catagories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" },
+    ],
+    CurrentCategory: "Current Category",
+  };
 
+  
+  changeCategory= (category)=>{
+
+    this.setState({ CurrentCategory: category.categoryName })
+  }//arrow function
   render() {
     return (
       <div>
@@ -20,9 +25,15 @@ export default class CategoryList extends Component {
         <h3>{this.state.counter}</h3>
         <ListGroup>
           {this.state.catagories.map((category) => (
-            <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem> //key kavramı=her bir elemanı diğerinden ayıracak ve eşşis olacak bir ıd ver.
+            <ListGroupItem
+              onClick={ ()=>this.changeCategory (category)}
+              key={category.categoryId}
+            >
+              {category.categoryName}
+            </ListGroupItem> //key kavramı=her bir elemanı diğerinden ayıracak ve eşşis olacak bir ıd ver.
           ))}
         </ListGroup>
+        <h4>{this.state.CurrentCategory}</h4>
       </div>
     );
   }
